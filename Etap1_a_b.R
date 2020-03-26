ebb12d632dbd2d93477b0711c11d716c21a57a30
emilia_kowal <249716@student.pwr.edu.pl>
2020-03-26 11:35:59 +0100

    Exploring types of features

--- Etap1_a_b.R
+++ Etap1_a_b.R
@@ -4,5 +4,15 @@ library(tidyr)
 dane <- read.csv(file="churn.txt")
 head(dane)
 
+#eksplorowanie struktury danych
+str(dane) 
+#zmienne area.code i phone powinny być jakościowe (tworzą id klienta)
+#według opisu danych zmienna Day.charge powinna być całkowita
+
+#można utworzyć zmienną jakościową (numer telefonu wraz z numerem kierunkowym): id_client
+#dane$Area.Code <- as.factor(dane$Area.Code)
+#ID <- unite(dane[,3:4], col="id_client", sep="-")
+#dane <- cbind(ID, dane)
+
 
 
