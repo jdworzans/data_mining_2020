c4a56dccd014775bb814983bff6212976ef83471
emilia_kowal <249716@student.pwr.edu.pl>
2020-04-04 17:35:44 +0200

    repairing types of saved data

--- Etap1_a_b.R
+++ Etap1_a_b.R
@@ -5,9 +5,11 @@ head(dane)
 paste("Liczba wierszy:", as.character(nrow(dane)))
 paste("Liczba kolumn:", as.character(ncol(dane)))
 str(dane) 
-#według opisu danych zmienna Day.charge powinna być całkowita (?)
 
-#usunięcie zmiennych Area.Code i Phone pełniących rolę ID klienta:
+#zmiana typu zmiennej Area.Code na factor
+dane$Area.Code <- as.factor(dane$Area.Code)
+
+#usunięcie zmiennej Phone pełniącej rolę id klienta:
 dane1 <- dane[,-4]
 
 #w zbiorze nie ma wartości brakujących
@@ -15,8 +17,7 @@ sum(is.na(dane)) #=0
 
 #zapisywanie wstępnie przeanalizowanych danych do pliku: churn1.txt
 
-write.table(dane1, file="churn1.txt", row.names=FALSE)
-
+saveRDS(dane1, file="cleaned_churn.rds")
 
 
 
